b1efa6da4bc4e2cdb8fa956ad57720ee35484537
RuichangBao <1294838346@qq.com>
2020-06-17 22:57:05 +0800

    dsafdsaf

--- LuaFramework/Assets/Scripts/Test.cs
+++ LuaFramework/Assets/Scripts/Test.cs
@@ -8,32 +8,34 @@ public class Test : MonoBehaviour
 {
 	public GameObject scrollView;
 	public GameObject item;
-	
-	
+	public Transform content;
+
 	// Use this for initialization
 	void Start()
 	{
-		GameObject srcoll = Resources.Load("UIScrollView") as GameObject;
-		scrollView = Instantiate(srcoll);
+		GameObject srcoll = Resources.Load("UIPanel") as GameObject;
+		scrollView = Instantiate(srcoll,Vector3.zero, Quaternion.identity);
 		scrollView.transform.SetParent(GameObject.Find("Canvas").transform);
+		scrollView.GetComponent<RectTransform>().offsetMax = Vector2.zero;
 		item = Resources.Load("Item") as GameObject;
+		content = scrollView.transform.Find("UIScrollView/Viewport/Content");
 		for (int i = 0; i < 10; i++)
 		{
 			GameObject obj = Instantiate(item);
 			GameObject objText= obj.transform.Find("Text").gameObject;
-			objText.GetComponent<Text>().text = "第" + i + "个";
-			int t = i;
+			int t = i + 1;
+			objText.GetComponent<Text>().text = "第" + t + "个";
 			obj.transform.Find("Button").gameObject.GetComponent<Button>().onClick.AddListener(()=> { Debug.LogError("你点击了第"+t+"个按钮"); });
-			obj.transform.SetParent(scrollView.transform);
+			obj.transform.SetParent(content);
 		}
 
-		int count=scrollView.transform.childCount;
+		int count = content.childCount;
 		int horizontal = count / 3;
 		if (count % 3 != 0)
 			horizontal++;
 		int h = horizontal * (200 + 10) + 30;
-		RectTransform rectTransform = scrollView.GetComponent<RectTransform>();
-		rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x,h);
+		RectTransform rectTransform = content.GetComponent<RectTransform>();
+		rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, h);
 
 	}
 
