f17faf04813cfa7d06a6c3f6058e5c61844f9f32
RuichangBao <1294838346@qq.com>
2020-08-26 22:21:15 +0800

    Xlua

---
+++ Xlua/Assets/Scripts/Init.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using XLua;
+public class Init : MonoBehaviour
+{
+	public static LuaEnv luaenv;
+	// Use this for initialization
+	void Start()
+	{
+	    luaenv = new LuaEnv();
+		string strLua= "print('hello world')";
+		luaenv.DoString(strLua);
+		luaenv.DoString(strLua);
+		luaenv.Dispose();
+		SceneManager.LoadScene("Test1");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ Xlua/Assets/Scripts/Test1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using XLua;
+
+public class Test1 : MonoBehaviour
+{
+
+	public InputField inputField1;
+	public InputField inputField2;
+	public Text textOperator;
+	public Text textResult;
+	public Button btn;
+	// Use this for initialization
+	void Start()
+	{
+		Init();
+		btn.onClick.AddListener(BtnOnClick);
+	}
+	
+	[Hotfix]
+	public void Init()
+	{
+		
+	}
+
+	private void BtnOnClick()
+	{
+		int num1 = int.Parse(inputField1.text);
+		int num2 = int.Parse(inputField2.text);
+		textResult.text = (num1 + num2).ToString();
+	}
+
+}
